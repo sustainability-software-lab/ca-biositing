64dd7aa516162bb2668b81040300d88e6a30b7a8
petercarbsmith <petersmith@lbl.gov>
2026-06-05 12:00:42 -0600

    attempting to not install qgis package to facilitate container build

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -3,5 +3,11 @@
 # For writing commands that will be executed after the container is created
 set -e
 
+# Increase file descriptor limit to prevent EMFILE errors
+ulimit -n 65536
+
 sudo chown vscode .pixi
-pixi install --all
+
+# Install core development and ETL environments
+# Skip resource-heavy environments like 'gis' (QGIS) to save space and time in Codespaces
+pixi install -e default -e etl -e webservice
