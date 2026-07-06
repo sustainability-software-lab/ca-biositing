e81b42ef4008eb2b66a04eef94c3ec28f1747fbc
petercarbsmith <petersmith@lbl.gov>
2026-07-06 12:21:11 -0600

    still cleaning

--- test_plotly_bdata.py
+++
@@ -1,9 +0,0 @@
-import json
-import plotly.graph_objects as go
-
-data = [{"x": {"dtype": "f8", "bdata": "FK5H4Xp0QEA="}, "y": {"dtype": "f8", "bdata": "N9BpA52WQkA="}, "type": "scatter"}]
-try:
-    fig = go.Figure(data=data)
-    print("Success!")
-except Exception as e:
-    print("Error:", e)
